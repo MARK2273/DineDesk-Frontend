86522de31d473be4d0b9e880246647c2b6d07f06
Ayush Mehta <mehtaayush781@gmail.com>
2025-02-18 22:11:24 +0530

    ADD : Add Draggable Item Functionality In Menu

--- src/Common/Components/Skeleton/Skeleton.tsx
+++ src/Common/Components/Skeleton/Skeleton.tsx
@@ -10,9 +10,9 @@ export const Skeleton: React.FC<SkeletonProps> = ({ className, count = 1 }) => {
   return (
     <Repeater count={count}>
       <div
-        className={`relative bg-Primary-100-opacity-90 rounded overflow-hidden ${className}`}
+        className={`relative bg-gray-200 rounded-md overflow-hidden animate-pulse ${className}`}
       >
-        <span className="absolute top-2/4 -translate-y-2/4 h-[200%] w-24 bg-white/70 blur-lg animate-skeleton -skew-x-[25deg] "></span>
+        <div className="absolute inset-0 bg-gradient-to-r from-gray-200 via-gray-300 to-gray-200 animate-shimmer"></div>
       </div>
     </Repeater>
   );
