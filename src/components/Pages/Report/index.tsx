f128757c77825f21f92be04fac461c5c4a4d46d6
Ayush Mehta <mehtaayush781@gmail.com>
2025-05-25 18:24:37 +0530

    ADD : Add Custom Date Range Picker And Integrate With Dashboard

--- src/components/Pages/Report/index.tsx
+++ src/components/Pages/Report/index.tsx
@@ -1,5 +1,25 @@
+import DateRangePicker, {
+  DateRange,
+} from "@dine-desk/Common/Components/FormField/DateRangePicker";
+import { useState } from "react";
+
 const Report = () => {
-  return <div>Report</div>;
+  const [dateRange, setDateRange] = useState<DateRange>({
+    startDate: null,
+    endDate: null,
+  });
+
+  return (
+    <div className="max-w-md mx-auto mt-10">
+      <DateRangePicker
+        startDate={dateRange.startDate}
+        endDate={dateRange.endDate}
+        onChange={(range) => {
+          setDateRange(range);
+        }}
+      />
+    </div>
+  );
 };
 
 export default Report;
